015f16cb5570cdbe63b888da951d323a33fe1c3c
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-08-31 22:36:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -46,11 +46,7 @@ end
 def reduce_to_all_true(source_array)
   counter = 0
   while counter < source_array.size do
-    return false if source_array[counter] != false
-    if element != true
-      return false
-    else 
-      return true
+    return false if source_array[counter] != true
   end
   return true
 end
